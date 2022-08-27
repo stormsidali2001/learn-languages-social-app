fb23797e1626ebbe128f722ebba8b0ef966e5480
stormsidali2001 <assoulsidali@gmail.com>
2022-08-27 14:00:23 -0700

    setting up the ionic angular frontend

---
+++ front-end-learn-languages-social-app/capacitor.config.ts
@@ -0,0 +1,10 @@
+import { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'front-end-learn-languages-social-app',
+  webDir: 'www',
+  bundledWebRuntime: false
+};
+
+export default config;

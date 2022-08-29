095e913206c0ddb7a1a3343200f17fd22db1acba
stormsidali2001 <assoulsidali@gmail.com>
2022-08-29 08:50:12 -0700

    adding the auth page (main module+routing module + component )

--- front-end-learn-languages-social-app/src/app/home/components/tab/tabs.component.ts
+++ front-end-learn-languages-social-app/src/app/home/components/tab/tabs.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { ModalController } from '@ionic/angular';
+import { ModalComponent } from '../start-post/modal/modal.component';
 
 @Component({
   selector: 'app-tabs',
@@ -7,8 +9,21 @@ import { Component, OnInit } from '@angular/core';
 })
 export class TabsComponent implements OnInit {
 
-  constructor() { }
+  constructor(public modalController:ModalController) { }
 
   ngOnInit() {}
+
+  async presentModal(){
+    const modal = await this.modalController.create({
+      component:ModalComponent,
+      cssClass:'my-custom-class2',
+    });
+    await modal.present();
+    const {data,role} = await modal.onDidDismiss();
+    if(data){
+      console.log('modal data:',data,'role',role)
+    }
+    console.log(role)
+  }
 
 }

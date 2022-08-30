2710e89c9d00182d333099fa429d25468e5d66c3
stormsidali2001 <assoulsidali@gmail.com>
2022-08-30 10:01:59 -0700

    updating components according to the role of the user behavioural subject (in the auth service)

--- front-end-learn-languages-social-app/src/app/home/components/profile-summary/profile-summary.component.ts
+++ front-end-learn-languages-social-app/src/app/home/components/profile-summary/profile-summary.component.ts
@@ -1,14 +1,61 @@
 import { Component, OnInit } from '@angular/core';
+import { take } from 'rxjs/operators';
+import { Role } from 'src/app/auth/models/user.model';
+import { AuthService } from 'src/app/auth/services/auth.service';
 
+type BannerColors = {
+  color1:string;
+  color2:string;
+  color3:string;
+}
 @Component({
   selector: 'app-profile-summary',
   templateUrl: './profile-summary.component.html',
   styleUrls: ['./profile-summary.component.scss'],
 })
 export class ProfileSummaryComponent implements OnInit {
+  bannerColors:BannerColors ={
+    color1:'#a0b4b7',
+    color2:'#dbe7e9',
+    color3:'#bfd3d6',
+    
+  };
 
-  constructor() { }
+  constructor(private authService:AuthService) { }
 
-  ngOnInit() {}
+  ngOnInit() {
+    this.authService.getUserRole.pipe(take(1)).subscribe((role:Role)=>{
+      console.log('role is',role)
+      this.bannerColors = this.getBannerColors(role);
+    })
+  }
+  private getBannerColors(role:Role):BannerColors{
+    switch (role) {
+    case 'user':
+        return {
+          color1:'#a0b4b7',
+          color2:'#dbe7e9',
+          color3:'#bfd3d6',
+        }
+      break;
+    case 'premium':
+      return {
+        color1:'#bc8f8f',
+        color2:'#c09999',
+        color3:'#ddadaf',
+      }
+        
+        break;
+      case 'admin':
+          return {
+            color1:'#daa528',
+            color2:'#f0e68c',
+            color3:'#fafad2',
+          }
+        break;
+    
+    
+    }
+  }
 
 }
